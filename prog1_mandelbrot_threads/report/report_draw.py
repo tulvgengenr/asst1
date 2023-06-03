be5b20ce534cc9a4f41be732e72f54a75966db55
tulvgengenr <871005218@qq.com>
2023-06-03 10:36:17 +0800

    add the speedup of the view2

--- prog1_mandelbrot_threads/report/report_draw.py
+++ prog1_mandelbrot_threads/report/report_draw.py
@@ -1,17 +1,22 @@
 import matplotlib.pyplot as plt
 
 # 定义 x 轴和 y 轴的数据
-x = [2, 3, 4, 5, 6, 8, 10, 12, 24, 48, 80, 100, 120, 150]
-y = [1.72, 1.59, 1.83, 2.18, 2.64, 3.38, 4.04, 4.38, 5.47, 6.22, 7.28, 7.72, 7.54, 7.30]
+x0 = [2, 3, 4, 5, 6, 8, 10]
+y0 = [1.72, 1.59, 1.83, 2.18, 2.64, 3.38, 4.04]
 
+x1 = [2, 3, 4, 5, 6, 10]
+y1 = [1.67, 1.99, 2.06, 2.21, 2.80, 3.85]
 # 绘制折线图
-plt.plot(x, y, marker='o')
+fig, ax = plt.subplots()
+plt.plot(x0, y0, marker='o', label='view1')
+plt.plot(x1, y1, marker='o', label='view2')
 
 # 设置图表标题和轴标签
 plt.title('')
 plt.xlabel('threads number')
 plt.ylabel('speed up')
 
+ax.legend()
 # 保存图表到本地文件
 plt.savefig('report1.png')
 
